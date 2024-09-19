64eff66819000121d660a53135e8e9b7c1a69035
Frankline Were <franklinegift@gmail.com>
2024-09-19 15:12:40 +0300

    create base classes/tables

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,5 +1,5 @@
 # imports
-from sqlalchemy import create_engine,
+from sqlalchemy import Date, create_engine
 from sqlalchemy import ForeignKey, Column, Integer, String
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,17 +8,43 @@ Base = declarative_base()
 
 # tables
 class Animal(Base):
-    pass
+    __tablename__ = "animals"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    species = Column(String())
+    age = Column(Integer())
+    enclosure_id = Column(Integer(), ForeignKey("enclosures.id")) # Foreign Key
+    enclosure = relationship("Enclosure", backref="animals") # One to Many Relationship
 
 class Enclosure(Base):
-    pass
+    __tablename__ = "enclosures"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    capacity = Column(Integer())
+    
 
 class Staff(Base):
-    pass
+    __tablename__ = "staffs"
 
-class Visitors(Base):
-    pass
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    role = Column(String())
 
-class Veterinary(Base):
-    pass
+class Visitor(Base):
+    __tablename__ = "visitors"
 
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    visit_date = Column(Date)
+
+class Vete(Base):
+   __tablename__ = "vets"
+   id = Column(Integer(), primary_key=True)
+   name = Column(String())
+   species_specialization = Column(String())
+
+
+engine = create_engine("sqlite:///zoo.db")
+Base.metadata.create_all(engine)
